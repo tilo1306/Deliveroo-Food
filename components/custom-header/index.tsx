d698a3d2241e8f574f782291c98053c2c7b2a899
Douglas dos Santos Oliveira <doug1306@gmail.com>
2025-06-01 20:35:55 -0300

    Enhance CustomHeader component with new layout and styles. Added location and profile buttons, updated styles for better alignment and spacing, and incorporated color constants for consistency.

--- components/custom-header/index.tsx
+++ components/custom-header/index.tsx
@@ -1,3 +1,5 @@
+import Colors from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
 import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
 
@@ -6,8 +8,24 @@ export default function CustomHeader() {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <TouchableOpacity>
-          <Image source={require('@/assets/images/bike.png')} />
-          <Text>Deliveroo</Text>
+          <Image
+            style={styles.image}
+            source={require('@/assets/images/bike.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.title}>Delivery · Now</Text>
+          <View style={styles.locationContainer}>
+            <Text style={styles.subtitle}>Carapicuiba, SP</Text>
+            <Ionicons
+              name="chevron-down-outline"
+              size={20}
+              color={Colors.primary}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
--- components/custom-header/styles.ts
+++ components/custom-header/styles.ts
@@ -1,3 +1,4 @@
+import Colors from '@/constants/Colors'
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
@@ -7,6 +8,38 @@ export const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     padding: 20,
+    flexDirection: 'row',
+    gap: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+  titleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
   },
 })
